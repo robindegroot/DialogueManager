4b41315e0d592d0a632a4601ffb05558ad5dc1e9
robindegroot <robindegroot.rdg@gmail.com>
2017-09-26 22:37:04 +0200

    working dialogue system

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    [SerializeField]
+    private float damping = 1;
+    public GameObject target;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+
+    }
+
+
+    void Update()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.LookAt(target.transform);
+        transform.position = target.transform.position - (rotation * offset);
+    }
+}
---
+++ Assets/DialogueStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueStart : MonoBehaviour {
+    public GameObject rob;
+	void Start () {
+        rob.SetActive(false);
+        Debug.Log("false");
+	}
+
+	void OnTriggerEnter (Collider other)
+    { 
+            rob.SetActive(true);
+        Debug.Log("true");
+    }
+}
